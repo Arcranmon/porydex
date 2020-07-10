a2091d8b7fc12d38284b3836874763efe17c6f58
Arcranmon <67716479+Arcranmon@users.noreply.github.com>
2020-07-10 00:01:50 -0400

    one last try

--- src/components/cards/Available.vue
+++ src/components/cards/Available.vue
@@ -2,7 +2,7 @@
         <v-row no-gutters >
             <v-col cols=12 class="top">
                 <v-expansion-panels flat>
-                    <v-expansion-panel>                            
+                    <v-expansion-panel style="border:1px solid black; border-radius:0">                            
                         <v-expansion-panel-header expand-icon='mdi-chevron-down' class="top"><b>Available To:</b></v-expansion-panel-header>
                             <v-expansion-panel-content class="bottom"><span v-for="n in namelist" :key="n">{{n}}<span v-if="n != namelist[namelist.length-1]">, </span></span>  </v-expansion-panel-content>
                     </v-expansion-panel>
@@ -51,9 +51,9 @@
     white-space: normal!important;
     color: black;
     font-family: "Courier New";  
-    border: 1px solid black;
     border-radius: 0px;
     font-size: 14px;
+    border:0px solid black;
     padding: 2px;
     box-sizing: border-box;
     text-align: left;
@@ -64,7 +64,8 @@
     border-radius: 0px;
     color: black;
     font-family: "Courier New";    
-    border: 1px solid black;
+    border:0px solid black;
+    border-top:1px solid black;
     font-size: 14px;
     padding: 0px;
     box-sizing: border-box;
