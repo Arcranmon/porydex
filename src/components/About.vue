b87ffd602aa7fa414855312994bf1e7d33ec67c5
Arcranmon <67716479+Arcranmon@users.noreply.github.com>
2020-07-15 21:15:01 -0400

    Navbar style refactoring

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,41 +1,39 @@
 <template>
-<div>
-      <v-dialog
-        v-model="dialog"
-        hide-overlay
-      >
-        <template v-slot:activator="{ on }">
-          <v-btn
-            color="red lighten-2"
-            dark
-            v-on="on"
+  <div>
+    <v-dialog v-model="dialog" hide-overlay>
+      <template v-slot:activator="{ on }">
+        <v-btn color="red lighten-2" dark v-on="on">
+          About
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title>About</v-card-title>
+        <v-card-text>
+          Welcome to the PoryDex, a free tool designed to make it easier to play
+          Pokemon Odyssey! Click on one of the buttons on the main page to get
+          started! Big thanks to the developers of the Pokemon Odyssey system
+          for making the system, and to the developers of COMP/CON, the digital
+          sheet for Lancer, for inspiration and guidance!
+          <br />
+          <br />
+          <a href="https://github.com/Arcranmon/porydex"
+            >Click here for more information and the source!</a
           >
-            About
-          </v-btn>
-        </template>
-
-        <v-card>
-          <v-card-title>About</v-card-title>
-          <v-card-text style="text-align:left">
-            Welcome to the PoryDex, a free tool designed to make it easier to play Pokemon  Odyssey! Click on one of the buttons on the main page to get started!
-
-            Big thanks to the developers of the Pokemon Odyssey system for making the system, and to the developers of COMP/CON, the digital sheet for Lancer, for inspiration and guidance!
-            <br> <br>
-            <a href="https://github.com/Arcranmon/porydex">Click here for more information and the source!</a>
-          </v-card-text>
-        </v-card>
-      </v-dialog>
- </div>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
-  import Vue from 'vue'
+import Vue from 'vue';
 
-  export default {
-    data () {
-      return {
-        dialog: false
-      }
-    }
-  }
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,37 +1,48 @@
 <template>
-    <div>
-        <router-link to="/" style="margin:10px"><span style="color:#ffb5a5;font-size:35px">Pory</span><span style="color:#7bc6ff;font-size:35px">Dex</span></router-link>
-        <div style="float:right;margin:10px">
-            <about />
-        </div>
-        
-    </div>
-
-    
+  <div class="nav--wrapper">
+    <router-link to="/"
+      ><span class="pory--start">Pory</span
+      ><span class="pory--end">Dex</span></router-link
+    >
+    <about class="about" />
+  </div>
 </template>
 
 <script>
-
-import About from './About'
+import About from './About';
 export default {
-    name: 'navbar',
-    data() {
-        return {
-            popupActive:false
-        }
-    },
-    components:{
-        About
-    }
-}
+  name: 'navbar',
+  data() {
+    return {
+      popupActive: false,
+    };
+  },
+  components: {
+    About,
+  },
+};
 </script>
 
-<style scoped>
-    a{
-        text-decoration: none!important;
-    }
-    *{
-        background-color: darkslategrey;
-        font-family: "Press Start 2P";
-    }
+<style scoped lang="scss">
+a {
+  text-decoration: none !important;
+}
+.nav--wrapper {
+  background-color: darkslategrey;
+  font-family: $font--gaming;
+  padding: $space--xs;
+  display: flex;
+  align-items: center;
+}
+.pory--start {
+  color: $color--pory;
+  font-size: 2em;
+}
+.pory--end {
+  color: $color--dex;
+  font-size: 2em;
+}
+.about {
+  margin-left: auto;
+}
 </style>
